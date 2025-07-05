4cfce13eb4e3ae217fb36e39c0e71ef346c12554
Sachin-20-s <dasarisachinraj09@gmail.com>
2025-07-05 19:59:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   process.env.CLIENT_ORIGIN,            
   'http://localhost:3000' ,
-  'https://seo-backend-jgpp.onrender.com'
 ];
 
 app.use(
