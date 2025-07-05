10a457d163011c5bf38f5d19f9c7f4af6017e47b
Sachin-20-s <dasarisachinraj09@gmail.com>
2025-07-05 20:09:37 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,20 +3,12 @@ const cors=require('cors');
 const app=express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-   process.env.CLIENT_ORIGIN,             
-  'http://localhost:3000' ,
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true); 
-      } else {
-        callback(new Error('Not allowed by CORS')); 
-      }
-    }
+    origin: [
+      process.env.CLIENT_ORIGIN,      
+      'http://localhost:3000'         
+    ]
   })
 );
 
