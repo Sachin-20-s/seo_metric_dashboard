6d2dabfadac3da42a02ebd701569b434470fbe2b
Sachin-20-s <dasarisachinraj09@gmail.com>
2025-07-06 00:13:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import DisplayCard from './components/DisplayCard'
 
 function App() {
   return (
-    <div className="flex flex-col justify-start p-5 items-center h-screen border-4">
+    <div className="flex flex-col justify-start p-5 items-center min-h-screen border-4">
       <BusinessForm/>
       <DisplayCard/>
     </div>
