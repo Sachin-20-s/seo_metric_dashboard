d9381fc03783c9078af4bb2a48db28fddd28440d
Sachin-20-s <dasarisachinraj09@gmail.com>
2025-07-07 12:00:26 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,10 @@ app.use((req, res, next) => {
 // Health check
 app.get('/health', (_req, res) => res.send('Backend is alive'));
 
-
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} | ${req.method} ${req.url}`);
+  next();
+});
 
 
 app.post('/business-data',(req,res)=>{
